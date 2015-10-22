9ea431e15542736b4ecbe17043d685db46d38ba1
srh-work <srodriguez@toth.cl>
2015-10-22 02:41:14 -0300

    0.0.0-dev

--- test/features/steps_definitions/functional_steps.js
+++ test/features/steps_definitions/functional_steps.js
@@ -1,3 +1,5 @@
+/*require("babel").transform("code", { blacklist: ["regenerator"] });*/
+
 import chai from "chai";
 
 const expect = chai.expect;
@@ -16,13 +18,20 @@ export default function () {
         next();
     });
 
-    this.Then(/^I should see "(.*)" as the page title$/, function(title, next) {
-    
-        this.driver
+    this.Then(/^I should see "(.*)" as the page title$/, function*(title, next) {
+        
+        let result = yield this.driver
+            .execute(() => {
+                return document.title;
+            });
+
+        console.log(result);
+        next();
+        /*this.driver
             .title(( err, res ) => {
                 expect( res.value ).to.equal( title );        
             })
-            .call( next );
+            .call( next );*/
     });
 
     this.Then(/^I should see the "perfect scrollbar" when the mouse pointer it's over the menu$/, function(next) {
